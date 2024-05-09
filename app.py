09401cde23a8c9d87b9b7f76e5d99c66b55dece2
siva-prasad123 <68691914+siva-prasad123@users.noreply.github.com>
2024-05-09 14:51:36 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+from fetch_cricket_data import fetch_live_cricket_data
+
+app = Flask(__name__)
+
+# Route to display live cricket data
+@app.route('/')
+def display_cricket_data():
+    # Fetch live cricket data
+    url = "https://crex.live/scoreboard/PBV/1L8/1st-Match/MP/MQ/fr-vs-mlt-1st-match-mdina-cup-t20-2024/live"
+    data = fetch_live_cricket_data(url)
+    return render_template('cricket_data.html', data=data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
